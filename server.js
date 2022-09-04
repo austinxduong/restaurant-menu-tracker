c346db58f4005f120ed907aa1117fe5a1936ff43
austinxduong <axduong2@gmail.com>
2022-09-04 13:57:51 -0700

    create folder 4 routes, + require them in server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const logger = require('morgan')
 const connectDB = require('./config/database')
 // const mainRoutes = require('./routes/main')
 // const todoRoutes = require('./routes/todos')
+const indexRouter = require('./routes/index')
 
 
 require('dotenv').config({path: './config/.env'})
