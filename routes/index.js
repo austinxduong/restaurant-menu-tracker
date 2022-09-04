9b0aeff037c3a3ca5c3ee497419d90346f2e1f68
austinxduong <axduong2@gmail.com>
2022-09-04 14:45:26 -0700

    complete + connect routes. then, setup controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const indexController = require('../controllers/index')
 //add controllers
 // const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.get('/', indexController.getIndex)
+router.post('/q', indexController.getSearch)
+
+module.exports = router
