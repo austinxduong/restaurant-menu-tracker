1c50cddbbf6da96a0f99fc507beea952a101b7db
austinxduong <axduong2@gmail.com>
2022-09-04 15:08:16 -0700

    add session

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const schemas = require('../models/schemas.js');
 module.exports = {
     getHome: async (req, res) => {
         let menu = schemas.menu
+        let sessionData = req.session
     }
 
     // getSearch:
