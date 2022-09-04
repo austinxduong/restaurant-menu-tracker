69a02f6a4e918997fa2d6b8e83ffe7922a721333
austinxduong <axduong2@gmail.com>
2022-09-04 14:19:38 -0700

    fixed

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-//add controllers
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
-
-
-module.exports = router
