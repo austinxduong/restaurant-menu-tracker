8ac3f5209f28059377c94f07a2e92132b3b7f254
austinxduong <axduong2@gmail.com>
2022-09-04 14:19:50 -0700

    fixed

---
+++ routes/login.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+//add controllers
+// const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+router.get('/', indexController.getHome)
+
+module.exports = router
