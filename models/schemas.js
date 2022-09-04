501dc94cafeb1b0c6bf88210248355b9e69a0080
austinxduong <axduong2@gmail.com>
2022-09-04 14:53:16 -0700

    setup Schemas

---
+++ models/schemas.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+let menuSchema = new mongoose.Schema({
+    name: {type:String, require: true},
+    icon: {type:String, require: true},
+    menuUrl: {type:String, require: true},
+    entryDate: {}
+})
