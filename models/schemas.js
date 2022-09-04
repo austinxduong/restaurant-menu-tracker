8020e984504d0fe07be09602ca32f2e9fffb5b7b
austinxduong <axduong2@gmail.com>
2022-09-04 14:56:00 -0700

    pop in userSchema

--- models/schemas.js
+++ models/schemas.js
@@ -4,5 +4,11 @@ let menuSchema = new mongoose.Schema({
     name: {type:String, require: true},
     icon: {type:String, require: true},
     menuUrl: {type:String, require: true},
-    entryDate: {}
+    entryDate: {type:Date, default:Date.now}
+})
+
+let userSchema = new mongoose.Schema({
+    email: {type:String, require: true},
+    password: {type:String, require: true},
+    entryDate: {type:Date, default:Date.now}
 })
