54fa2ecbef43b6e43dcc2402ee6ba39e81f43b32
austinxduong <axduong2@gmail.com>
2022-09-04 14:24:55 -0700

    adding controller routing logic - for logins

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,11 @@ const router = express.Router()
 //add controllers
 // const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/', indexController.getHome)
+router.get('/', loginController.getLogin)
+router.get('/new-acct', loginController.getSignup)
+router.get('/logout', loginController.getLogout)
+router.post('/', loginController.postLogin)
+router.post('/new', loginController.postSignup)
+
 
 module.exports = router
